e2e39d8d52d80a92b1dacdf993327eee91bd956a
mikelaud <mmx.mmx@gmail.com>
2019-11-25 00:32:38 +0300

    add var scope

---
+++ ui/spa/2_reintro-js/1b_var-scope-escaped.js
@@ -0,0 +1,12 @@
+function prison() {
+    prisoner_1 = "I escaped !";
+    var prisoner_2 = "I am isolated !";
+}
+
+prison();
+
+// I escaped !
+console.log(prisoner_1);
+
+// Error
+console.log(prisoner_2);
